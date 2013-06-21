46a8c677c5f0f6f5ac0ee1c473518646529ccbc4
Marc Lallaouret <mlallaouret@excilys.com>
2013-06-21 17:26:55 +0200

    ajout de nodes, affichages info nodes

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,16 +1,53 @@
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.RelationshipType;
+import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.neo4j.kernel.EmbeddedGraphDatabase;
 
 
 public class Launcher {
-
+	
+	private static enum RelTypes implements RelationshipType
+	{
+	    HAS_SUBFOLDER, HAS_COURSE,HAS_QUESTION,HAS_EXERCISE
+	}
+	
 	public static void main (String...args) {
 		System.out.println("MARC ! ");
+		Node firstNode;
+		Node secondNode;
+		Relationship relationship;
+		EmbeddedGraphDatabase graphDb;
 		
-		EmbeddedGraphDatabase graphDb = (EmbeddedGraphDatabase) new GraphDatabaseFactory().newEmbeddedDatabase( args[0]);
+		//Creation graphe
+		graphDb = (EmbeddedGraphDatabase) new GraphDatabaseFactory().newEmbeddedDatabase( args[0]);
 		registerShutdownHook( graphDb );
 		System.out.println(graphDb);
+		
+		Transaction tx = graphDb.beginTx();
+		try
+		{
+			firstNode = graphDb.createNode();
+			firstNode.setProperty( "message", "Hello, " );
+			secondNode = graphDb.createNode();
+			secondNode.setProperty( "message", "World!" );
+			 
+			relationship = firstNode.createRelationshipTo( secondNode, RelTypes.HAS_COURSE );
+			relationship.setProperty( "message", "brave Neo4j " );
+		    tx.success();
+		}
+		finally
+		{
+		    tx.finish();
+		}
+		
+		System.out.print( firstNode.getProperty( "message" ) );
+		System.out.print( relationship.getProperty( "message" ) );
+		System.out.print( secondNode.getProperty( "message" ) );
+		
+		graphDb.shutdown();
 	}
 	
 	private static void registerShutdownHook( final GraphDatabaseService graphDb )
@@ -23,7 +60,9 @@ public class Launcher {
 	        @Override
 	        public void run()
 	        {
-	            graphDb.shutdown();
+	            if(graphDb!=null) {
+	            	graphDb.shutdown();
+	            }
 	        }
 	    } );
 	}
