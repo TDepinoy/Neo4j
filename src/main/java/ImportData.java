7019e98d67328ce976a83f6a7619bca1af0d8aae
Marc Lallaouret <mlallaouret@gmail.com>
2013-06-24 14:49:08 +0200

    Test premier graphe

--- src/main/java/ImportData.java
+++ src/main/java/ImportData.java
@@ -1,21 +1,66 @@
 import java.io.File;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+
+import org.neo4j.graphdb.DynamicLabel;
+import org.neo4j.graphdb.Label;
 import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Relationship;
 import org.neo4j.kernel.EmbeddedGraphDatabase;
 
 
 public class ImportData {
-
+	
+	private static final Pattern regexCours = Pattern.compile("c\\d+-.*");
+	private static final Pattern regexExercise = Pattern.compile("e\\d+-.*");
+	private static final Pattern regexQuestion = Pattern.compile("q\\d+-.*");
 	
 	public void importDataFromFolder(File file, EmbeddedGraphDatabase graph) {
 		
 		Node node;
-		Node secondNode;
-		Relationship relationship;
+		Label labelNode;
 		
 		//Creation du noeud root
+		labelNode = DynamicLabel.label(Launcher.NodeTypes.ROOT.toString());
+		node = graph.createNode(labelNode);
+		System.out.println("root créé");
+		//Parcours de l arborescence et creation des noeuds
+		parcoursFolder(file, node, graph);
 		
+	}
+	
+	private void parcoursFolder(File file, Node nodeParent, EmbeddedGraphDatabase graph) {
+		Node node;
+		Label labelNode;
+		File []list = file.listFiles();
 		
+		for (File f : list){
+			if (f.isDirectory()){
+				labelNode = DynamicLabel.label(f.getName());
+				node = graph.createNode(labelNode);
+				Launcher.RelTypes type = getRelTypes(f);
+				nodeParent.createRelationshipTo( node, type);
+				if(type == Launcher.RelTypes.HAS_SUBFOLDER){
+					parcoursFolder(f, node, graph);
+				}
+			}
+		}
+	}
+	
+	private Launcher.RelTypes getRelTypes(File file){
+		Matcher matcher = regexCours.matcher(file.getName());
+		if(matcher.find()){
+			return Launcher.RelTypes.HAS_COURSE;
+		}
+		matcher = regexExercise.matcher(file.getName());
+		if(matcher.find()){
+			return Launcher.RelTypes.HAS_EXERCISE;
+		}
+		matcher = regexQuestion.matcher(file.getName());
+		if(matcher.find()){
+			return Launcher.RelTypes.HAS_QUESTION;
+		}
+		
+		return Launcher.RelTypes.HAS_SUBFOLDER;
 	}
 }
--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,4 +1,7 @@
+import java.io.File;
+
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Label;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.RelationshipType;
@@ -16,42 +19,54 @@ public class Launcher {
 	
 	public static enum NodeTypes implements RelationshipType
 	{
-	    ROOT, FOLDER, COURSE, QUESTION, EXERCISE
+	    ROOT("root"), FOLDER("folder"), COURSE("course"), QUESTION("question"), EXERCISE("exercise");
+	    
+	    private String name;
+	    
+	    private NodeTypes(String name){
+	    	this.name=name;
+	    }
+	    public String toString(){
+	    	return this.name;
+	    }
 	}
 	
 	public static void main (String...args) {
-		System.out.println("MARC ! ");
-		Node firstNode;
+		/*Node firstNode;
 		Node secondNode;
-		Relationship relationship;
+		Relationship relationship;*/
 		EmbeddedGraphDatabase graphDb;
 		
 		//Creation graphe
-		graphDb = (EmbeddedGraphDatabase) new GraphDatabaseFactory().newEmbeddedDatabase( args[0]);
+		graphDb = (EmbeddedGraphDatabase) new GraphDatabaseFactory().newEmbeddedDatabase(args[0]);
 		registerShutdownHook( graphDb );
 		System.out.println(graphDb);
 		
-		//Création des nodes
+		//Creation des nodes
 		Transaction tx = graphDb.beginTx();
 		try
 		{
-			firstNode = graphDb.createNode();
+			/*firstNode = graphDb.createNode();
 			firstNode.setProperty( "message", "Hello, " );
 			secondNode = graphDb.createNode();
 			secondNode.setProperty( "message", "World!" );
 			 
 			relationship = firstNode.createRelationshipTo( secondNode, RelTypes.HAS_COURSE );
 			relationship.setProperty( "message", "brave Neo4j " );
-		    tx.success();
+		    tx.success();*/
+			
+			ImportData importUtil = new ImportData();
+			System.out.println(args[1]);
+			importUtil.importDataFromFolder(new File(args[1]), graphDb);
 		}
 		finally
 		{
 		    tx.finish();
 		}
 		
-		System.out.print( firstNode.getProperty( "message" ) );
+		/*System.out.print( firstNode.getProperty( "message" ) );
 		System.out.print( relationship.getProperty( "message" ) );
-		System.out.print( secondNode.getProperty( "message" ) );
+		System.out.print( secondNode.getProperty( "message" ) );*/
 		
 		graphDb.shutdown();
 	}
